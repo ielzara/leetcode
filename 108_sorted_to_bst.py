01daf9627d1d88e5090c8691c590f1f50211d105
Elzara Turakulova <endjik@gmail.com>
2023-11-27 22:38:08 -0800

    expanded solution to 108 with extra methods to convert TreeNode to BST list

--- 108_sorted_to_bst.py
+++ 108_sorted_to_bst.py
@@ -1,5 +1,7 @@
 # Definition for a binary tree node.
 from typing import List
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -16,7 +18,32 @@ class Solution:
 
         root = TreeNode(nums[mid])
         root.left = self.sortedArrayToBST(nums[:mid])  # left subtree
-        root.right = self.sortedArrayToBST(nums[mid + 1 :]) # right subtree
+        root.right = self.sortedArrayToBST(nums[mid + 1 :])  # right subtree
         return root
 
-    
+#     def tree_to_list(self, root: TreeNode):
+#         lists = []
+#         self.tree_to_lists(root, lists, 0)
+
+#         ret = []
+#         for l in lists:
+#             while l and l[-1] is None:
+#                 l.pop()
+#             ret += l
+#         return ret
+
+#     def tree_to_lists(self, root: TreeNode, lists: List[List[int]], level: int):
+#         while len(lists) < level + 1:
+#             lists.append([])
+#         if not root:
+#             lists[level].append(None)
+#             return
+#         lists[level].append(root.val)
+#         self.tree_to_lists(root.left, lists, level + 1)
+#         self.tree_to_lists(root.right, lists, level + 1)
+
+
+# x = Solution()
+# root = x.sortedArrayToBST([-10, -3, 0, 5, 9])
+
+# print(x.tree_to_list(root))
